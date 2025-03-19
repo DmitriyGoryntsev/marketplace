eaabed20477b9bbd9b1f628e27fb29216f4a57b6
Your NameGDA35 <goryntsev510@mail.ru>
2025-03-19 23:47:15 +0300

    fix: fix configuration file

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,49 +2,51 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
-	"github.com/DmitriyGoryntsev/marketplace/pkg/postgres"
+	"github.com/go-playground/validator/v10"
 	"github.com/ilyakaznacheev/cleanenv"
-	"go.uber.org/zap"
 )
 
 type HTTPServer struct {
-	Host string `yaml:"host" env:"HOST" env-required:"true" env-default:"localhost"`
-	Port string `yaml:"port" env:"PORT" env-required:"true" env-default:"8080"`
+	Host string `yaml:"host" env:"HOST" env-default:"localhost" validate:"required"`
+	Port string `yaml:"port" env:"PORT" env-default:"8080" validate:"required,numeric"`
 }
+
+type DBConfig struct {
+	Host     string `yaml:"host" env:"DB_HOST" env-default:"localhost" validate:"required"`
+	Port     int    `yaml:"port" env:"DB_PORT" env-default:"5432" validate:"required,numeric"`
+	User     string `yaml:"user" env:"DB_USER" env-default:"user" validate:"required"`
+	Password string `yaml:"password" env:"DB_PASSWORD" env-default:"password" validate:"required"`
+	Name     string `yaml:"name" env:"DB_NAME" env-default:"marketplace" validate:"required"`
+}
+
 type Config struct {
-	Env string `yaml:"env" env:"ENV" env-required:"true" env-default:"prod"`
-
-	HTTPServer HTTPServer `yaml:"http_server"`
-
-	DBConfig postgres.DBConfig `yaml:"db"`
+	Env        string     `yaml:"env" env:"ENV" env-default:"prod" validate:"oneof=dev prod test"`
+	HTTPServer HTTPServer `yaml:"http_server" validate:"required"`
+	DB         DBConfig   `yaml:"db" validate:"required"`
 }
 
 func New() (*Config, error) {
-	var configPath string
-
-	configPath = os.Getenv("CONFIG_PATH")
+	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		flags := flag.String("config", "", "path to config file")
+		flag.StringVar(&configPath, "config", "./config/local.yaml", "path to config file")
 		flag.Parse()
-
-		configPath = *flags
-
-		if configPath == "" {
-			zap.L().Fatal("config path is empty")
-		}
 	}
 
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		zap.L().Fatal("config file does not exist")
+		return nil, fmt.Errorf("config file does not exist: %s", configPath)
 	}
 
 	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to read config: %w", err)
+	}
 
-	err := cleanenv.ReadConfig(configPath, &cfg)
-	if err != nil {
-		zap.L().Fatal("failed to read config", zap.Error(err))
+	validate := validator.New()
+	if err := validate.Struct(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to validate config: %w", err)
 	}
 
 	return &cfg, nil
